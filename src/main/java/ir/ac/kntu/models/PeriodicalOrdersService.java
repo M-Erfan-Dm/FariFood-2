bf0c5689475210cc58ce64ab99cf2437719e37e5
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-29 22:51:51 +0430

    getActiveOrders in PeriodicalOrdersService

--- src/main/java/ir/ac/kntu/models/PeriodicalOrdersService.java
+++ src/main/java/ir/ac/kntu/models/PeriodicalOrdersService.java
@@ -1,7 +1,9 @@
 package ir.ac.kntu.models;
 
+import java.time.Period;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class PeriodicalOrdersService extends OrdersService<PeriodicalOrder> {
@@ -12,5 +14,11 @@ public class PeriodicalOrdersService extends OrdersService<PeriodicalOrder> {
     public List<PeriodicalOrder> getListOfOrdersByTimePeriod(TimePeriod timePeriod) {
         return getOrders().stream().filter(periodicalOrder -> periodicalOrder.getTimePeriod().equals(
                 timePeriod)).collect(Collectors.toList());
+    }
+
+    public List<PeriodicalOrder> getActiveOrders(TimePeriod timePeriod){
+        return getListOfOrdersByTimePeriod(timePeriod).stream().filter(
+                periodicalOrder -> periodicalOrder.getOrderState()!=OrderState.DELIVERED)
+                .collect(Collectors.toList());
     }
 }
