8dbcf25919a3423928684cf9ca3a906c766a5504
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-31 02:23:02 +0430

    setup ListSorting

---
+++ src/main/java/ir/ac/kntu/utils/ListSorting.java
@@ -0,0 +1,33 @@
+package ir.ac.kntu.utils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class ListSorting {
+
+    public static <T> List<T> sortList(List<T> list, int count , boolean isAscending, Function<T,Double> mapper){
+        List<T> sortedList = new ArrayList<>(list);
+        sortedList.sort((o1, o2) -> {
+            if (isAscending){
+                return compareNumbers(mapper.apply(o1),mapper.apply(o2));
+            }else {
+                return compareNumbers(mapper.apply(o2),mapper.apply(o1));
+            }
+        });
+        if (sortedList.size()>count){
+            sortedList = sortedList.subList(0,count);
+        }
+        return sortedList;
+    }
+
+    private static int compareNumbers(double one, double two) {
+        if (one < two) {
+            return -1;
+        } else if (one > two) {
+            return 1;
+        }
+        return 0;
+    }
+}
