09524c8dfafcb143f5423afa060b3b635042d8aa
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-30 16:12:49 +0430

    override getAdditionalPrices in Restaurant

--- src/main/java/ir/ac/kntu/models/Restaurant.java
+++ src/main/java/ir/ac/kntu/models/Restaurant.java
@@ -2,22 +2,22 @@ package ir.ac.kntu.models;
 
 import ir.ac.kntu.db.CouriersDB;
 
-public class Restaurant extends Shop<OrdersService<Order>> {
+public class Restaurant extends Shop<Order,OrdersService<Order>> {
 
     private FoodMenu foodMenu;
 
     private RestaurantPriceType priceType;
 
     public Restaurant(int id, String name, String address, Schedule schedule,
-                      CouriersDB hiredCouriers, OrdersService<Order> ordersService,
+                      CouriersDB hiredCouriers, OrdersService<Order> ordersService, int deliveryPrice,
                       FoodMenu foodMenu, RestaurantPriceType priceType) {
-        super(id, name, address, schedule, hiredCouriers, ordersService);
+        super(id, name, address, schedule, hiredCouriers, ordersService,deliveryPrice);
         this.foodMenu = foodMenu;
         this.priceType = priceType;
     }
 
-    public Restaurant(String name, String address, Schedule schedule, RestaurantPriceType priceType) {
-        super(name, address, schedule);
+    public Restaurant(String name, String address, Schedule schedule, RestaurantPriceType priceType, int deliveryPrice) {
+        super(name, address, schedule,deliveryPrice);
         this.priceType = priceType;
     }
 
@@ -37,6 +37,11 @@ public class Restaurant extends Shop<OrdersService<Order>> {
         this.priceType = priceType;
     }
 
+
+    @Override
+    public int getAdditionalPrices(Order order) {
+        return getDeliveryPrice();
+    }
 
     @Override
     public String toString() {
