1f92216633b4f5b13d168e05c7146a31de018d82
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-30 22:15:22 +0430

    setup FruitShop

---
+++ src/main/java/ir/ac/kntu/models/FruitShop.java
@@ -0,0 +1,36 @@
+package ir.ac.kntu.models;
+
+import ir.ac.kntu.db.CouriersDB;
+import ir.ac.kntu.service.FruitShopPeriodsService;
+import ir.ac.kntu.service.PeriodsService;
+import ir.ac.kntu.service.PeriodsServiceImpl;
+
+import java.util.List;
+
+public class FruitShop extends Shop<PeriodicalOrder,PeriodicalOrdersService> {
+
+    private final FruitShopPeriodsService periodsService;
+
+    public FruitShop(int id, String name, String address, Schedule schedule,
+                     CouriersDB hiredCouriers, PeriodicalOrdersService ordersService,
+                     int deliveryPrice, int periodBasePrice, int fruitKGLimit) {
+        super(id, name, address, schedule, hiredCouriers, ordersService, deliveryPrice);
+        periodsService = new FruitShopPeriodsService(this,periodBasePrice,fruitKGLimit);
+    }
+
+    public FruitShop(String name, String address, Schedule schedule, int deliveryPrice
+            , int periodBasePrice, int fruitKGLimit) {
+        super(name, address, schedule, deliveryPrice);
+        periodsService = new FruitShopPeriodsService(this,periodBasePrice,fruitKGLimit);
+    }
+
+    public FruitShopPeriodsService getPeriodsService() {
+        return periodsService;
+    }
+
+    @Override
+    public int getAdditionalPrices(PeriodicalOrder order) {
+        return getDeliveryPrice() + periodsService.getPriceOfPeriod(order.getTimePeriod());
+    }
+
+}
