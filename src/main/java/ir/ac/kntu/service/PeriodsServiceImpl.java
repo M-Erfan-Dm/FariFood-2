769764f4228dac1db70c0141c491f97500341aea
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-05-30 22:10:08 +0430

    update getActivePeriods in PeriodsServiceImpl

--- src/main/java/ir/ac/kntu/service/PeriodsServiceImpl.java
+++ src/main/java/ir/ac/kntu/service/PeriodsServiceImpl.java
@@ -1,11 +1,12 @@
 package ir.ac.kntu.service;
 
+import ir.ac.kntu.models.Order;
 import ir.ac.kntu.models.TimePeriod;
 
 import java.util.List;
 
 public interface PeriodsServiceImpl {
-    List<TimePeriod> getActivePeriods();
+    List<TimePeriod> getActivePeriods(Order order);
     int getPriceOfPeriod(TimePeriod timePeriod);
     int getPeriodCapacity(TimePeriod timePeriod);
 }
