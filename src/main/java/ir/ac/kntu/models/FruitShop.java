be26453a35ec5e6e129d9f2e6c6c9e4f6a6712dc
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-06-06 14:13:35 +0430

    add food menu to Supermarket, FruitShop

--- src/main/java/ir/ac/kntu/models/FruitShop.java
+++ src/main/java/ir/ac/kntu/models/FruitShop.java
@@ -7,10 +7,13 @@ public class FruitShop extends Shop<PeriodicalOrder,PeriodicalOrdersService> {
 
     private final FruitShopPeriodsService periodsService;
 
-    public FruitShop(int id, Owner owner,String name, String address, Schedule schedule,
+    private CountableFoodMenu foodMenu;
+
+    public FruitShop(int id, Owner owner, String name, String address, Schedule schedule,
                      CouriersDB hiredCouriers, PeriodicalOrdersService ordersService,
-                     int deliveryPrice, int periodBasePrice, int fruitKGLimit,ShopPriceType priceType) {
+                     int deliveryPrice, int periodBasePrice, int fruitKGLimit, ShopPriceType priceType, CountableFoodMenu foodMenu) {
         super(id, owner, name, address, schedule, hiredCouriers, ordersService, deliveryPrice, priceType);
+        this.foodMenu = foodMenu;
         periodsService = new FruitShopPeriodsService(this,periodBasePrice,fruitKGLimit);
     }
 
@@ -22,6 +25,14 @@ public class FruitShop extends Shop<PeriodicalOrder,PeriodicalOrdersService> {
 
     public FruitShopPeriodsService getPeriodsService() {
         return periodsService;
+    }
+
+    public CountableFoodMenu getFoodMenu() {
+        return foodMenu;
+    }
+
+    public void setFoodMenu(CountableFoodMenu foodMenu) {
+        this.foodMenu = foodMenu;
     }
 
     @Override
--- src/main/java/ir/ac/kntu/models/Supermarket.java
+++ src/main/java/ir/ac/kntu/models/Supermarket.java
@@ -11,12 +11,15 @@ public class Supermarket extends Shop<PeriodicalOrder,PeriodicalOrdersService> {
 
     private PremiumCustomersService premiumCustomersService;
 
+    private CountableFoodMenu foodMenu;
+
     public Supermarket(int id, Owner owner, String name, String address, Schedule schedule,
                        CouriersDB hiredCouriers, PeriodicalOrdersService ordersService,
-                       int deliveryPrice, int periodBasePrice, PremiumCustomersService premiumCustomersService,ShopPriceType priceType) {
+                       int deliveryPrice, int periodBasePrice, PremiumCustomersService premiumCustomersService, ShopPriceType priceType, CountableFoodMenu foodMenu) {
         super(id, owner,name, address, schedule, hiredCouriers, ordersService, deliveryPrice, priceType);
         periodsService = new SupermarketPeriodsService(this, periodBasePrice);
         this.premiumCustomersService = premiumCustomersService;
+        this.foodMenu = foodMenu;
     }
 
     public Supermarket(String name, String address, Schedule schedule, int deliveryPrice, int periodBasePrice,ShopPriceType priceType) {
@@ -26,6 +29,14 @@ public class Supermarket extends Shop<PeriodicalOrder,PeriodicalOrdersService> {
 
     public SupermarketPeriodsService getPeriodsService() {
         return periodsService;
+    }
+
+    public CountableFoodMenu getFoodMenu() {
+        return foodMenu;
+    }
+
+    public void setFoodMenu(CountableFoodMenu foodMenu) {
+        this.foodMenu = foodMenu;
     }
 
     @Override
