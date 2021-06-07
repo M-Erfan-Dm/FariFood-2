b9a7a43aab9f83443e5812e819cf3aac629d2501
M-Erfan-Dm <erfan.dtm@gmail.com>
2021-06-08 03:15:32 +0430

    setup ShowOrdersMenu

---
+++ src/main/java/ir/ac/kntu/menu/admin/ShowOrdersMenu.java
@@ -0,0 +1,77 @@
+package ir.ac.kntu.menu.admin;
+
+import ir.ac.kntu.db.RestaurantsDB;
+import ir.ac.kntu.db.ShopsDB;
+import ir.ac.kntu.menu.Menu;
+import ir.ac.kntu.models.*;
+
+import java.util.ArrayList;
+
+public class ShowOrdersMenu extends Menu {
+
+    private final ShopsDB<? extends Shop<? extends OrdersService<? extends Order>>> shopsDB;
+
+    public ShowOrdersMenu(ShopsDB<? extends Shop<? extends OrdersService<? extends Order>>> shopsDB) {
+        this.shopsDB = shopsDB;
+    }
+
+    @Override
+    public void show() {
+        ShowOrdersOption option ;
+        while ((option = printShowOrderOptions())
+        != ShowOrdersOption.BACK) {
+            if (option != null) {
+                switch (option) {
+                    case SHOW_ALL:
+                        showAllOrders();
+                        break;
+                    case SHOW_BY_ID:
+                        showOrderById();
+                        break;
+                    case SHOW_BY_STATE:
+                        showOrderByState();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private ShowOrdersOption printShowOrderOptions() {
+        printEnumOptions(ShowOrdersOption.class);
+        System.out.println("Enter your choice");
+        return getOption(ShowOrdersOption.class);
+    }
+
+    private void showAllOrders() {
+        printList(new ArrayList<>(shopsDB.getAllOrders()),"orders");
+    }
+
+    private void showOrderById() {
+        int id = getId();
+        Shop<?> shop = shopsDB.getShopByOrderId(id);
+        if (shop == null) {
+            System.out.println("Order not found");
+            return;
+        }
+        Order order = shop.getOrdersService().getOrderById(id);
+        System.out.println("Order : " + order);
+    }
+
+    private void showOrderByState() {
+        OrderState orderState = getOrderState();
+        if (orderState == null) {
+            return;
+        }
+        printList(new ArrayList<>( new OrdersService<>(shopsDB.getAllOrders()).getOrdersByState(orderState))
+                ,"orders");
+    }
+
+    private OrderState getOrderState() {
+        OrderState.printOptions();
+        System.out.println("Enter your choice :");
+        return getOption(OrderState.class);
+    }
+}
+
---
+++ src/main/java/ir/ac/kntu/menu/admin/ShowOrdersOption.java
@@ -0,0 +1,8 @@
+package ir.ac.kntu.menu.admin;
+
+public enum ShowOrdersOption {
+    SHOW_ALL,
+    SHOW_BY_ID,
+    SHOW_BY_STATE,
+    BACK
+}
